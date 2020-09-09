145b122e1acff9d1eb2ca84d683c7754d6237516
Agmar Torres <agmartorres@gmail.com>
2020-09-09 00:59:44 -0300

    header and search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,34 @@
 import React from "react";
 import "./Home.css";
-
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import Search from "./Search";
 function Home() {
   return (
     <div className="home">
-      <h1>This is HomePage</h1>
       <div className="home__header">
-        <Link to="/about">About</Link>
-        <Link to="/store">Store</Link>
+        <div className="home__headerLeft">
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
+        </div>
+        <div className="home__headerRight">
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Image</Link>
+          <AppsIcon />
+
+          <Avatar />
+        </div>
       </div>
+
       <div className="home__body">
-        <Link to="/gmail">Gmail</Link>
-        <Link to="/images">Image</Link>
+        <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt=""
+        />
+        <div className="home__inputContainer">
+          <Search />
+        </div>
       </div>
     </div>
   );
